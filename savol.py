162044815108ca6015be959210d608f808135619
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2020-12-24 20:35:33 +0500

    qo'shimcha fayl

---
+++ savol.py
@@ -0,0 +1,16 @@
+class Savol:
+    def __init__(self,question,answer):
+        self.question = question
+        self.answer = answer
+
+    def natija(self,a):
+        if a == self.answer:
+            z="to'xriii"
+        else:
+            z="xatoooo"
+        print(z)
+
+
+
+
+        
